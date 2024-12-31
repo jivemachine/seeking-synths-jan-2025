c42f682511f993f0603aff5f3ff3540a73710f2c
jivemachine <the.jeremy.cobb@gmail.com>
2024-12-31 15:15:18 -0600

    add about us page and route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('seeking-synths');
 });
+
+
+Route::get('/about-us', function () {
+    return view('about-us');
+});
