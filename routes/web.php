7adcdd94bb33f6f732ab2f80aa4383aefd5e0f39
jivemachine <the.jeremy.cobb@gmail.com>
2025-01-02 14:47:42 -0600

    add robots.txt route to serve custom robots.txt file

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('seeking-synths');
 });
+
+// Robots.txt page
+Route::get('/robots.txt', function () {
+    return response()->view('robots')->header('Content-Type', 'text/plain');
+});
