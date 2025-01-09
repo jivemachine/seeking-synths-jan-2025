4f3a59c31863c1d50ff52cbc706b4a620986aea2
Jeremy <the.jeremy.cobb@gmail.com>
2025-01-09 17:20:51 -0600

    feat: enhance Tailwind CSS configuration with additional font sizes and max widths

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,17 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                helvetica: ["Helvetica", "sans-serif"],
+                helvetica: ['Helvetica', 'sans-serif'],
+            },
+            fontSize: {
+                '6xl': ['3.75rem', { lineHeight: '4.3119rem' }],
+                '4xl': ['2rem', { lineHeight: '2.3rem' }],
+                'xl': ['1.25rem', { lineHeight: '1.4375rem' }],
+            },
+            maxWidth: {
+                '2xl': '606px',
+                '3xl': '891px',
+                'screen-xl': '1440px',
             },
         },
     },
